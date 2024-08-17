6046788bba8cd99cbbb67ec958a16535bd33596d
jasurgraduate <jasur@graduate.org>
2024-08-17 10:17:29 +0500

    new small features :)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Chess Game | 2-sided</h1>
+        <h1>Chess Game | 2-sided </h1>
       </header>
       <main className="App-content">
         <ChessGame />
